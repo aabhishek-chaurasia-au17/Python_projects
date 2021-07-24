00dbae762d8e74b3fe4061b990b8ee3737e0447a
aabhishek-chaurasia-au17 <abhishekc838@gmail.com>
2021-07-24 15:10:24 +0530

    new pro

---
+++ swigg/Apoorva.py
@@ -0,0 +1,4 @@
+class Apoorva:
+
+    def food_item(self):
+        self.menu_B = {'Roti':'10','Dal':'50','Jeera rice':'70','Lemon rice':'30','Sweet Rice':'80','Puri':'45'}
---
+++ swigg/district.py
@@ -0,0 +1,4 @@
+class District:
+
+    def food_item(self):
+        self.menu_C = {'Chicken Biriyani':'100','Kabab':'150','Chicken Momos':'70','Matan Soop':'70','Sherva':'40','Puri':'60'}
